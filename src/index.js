e37ccde39c99c28e2e71d2da77f89893cc820228
Sufyan <Sufyansial112@gmail.com>
2023-07-13 00:02:36 +0500

    profile-card-final

--- src/index.js
+++ src/index.js
@@ -11,20 +11,47 @@ function App() {
         {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
-        <SkillList />
+        <Skill />
       </div>
     </div>
   );
 }
 
 function Avatar() {
-  return <h1>hello</h1>;
+  return <img className="avatar" src="/images.jpg"></img>;
 }
 function Intro() {
-  return <p>hi</p>;
+  return (
+    <div>
+      <h1>Sufyan Sial</h1>
+      <p>
+        Front-end web developer with enhance knowledge in development and have
+        learned html css javacript and react.js. This profile card is my 2nd
+        project using react.js. I am eager to learn and explore more from it and
+        deermined to achieve good position in this field
+      </p>
+    </div>
+  );
 }
-function SkillList() {
-  return <span>yo</span>;
+
+function Skill() {
+  return (
+    <div className="skill-list">
+      <SkillList color="red" name="Html" level="expert" />
+      <SkillList color="green" name="CSS" level="expert" />
+      <SkillList color="blue" name="Javascript" level="intermediate" />
+      <SkillList color="pink" name="React.js" level="Begineer" />
+    </div>
+  );
+}
+
+function SkillList(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <h3>{props.name}</h3>
+      <span>{props.level}</span>
+    </div>
+  );
 }
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
