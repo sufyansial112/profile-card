57c4c5872734ec1c592259502f5a828fdc23e188
Sufyan <Sufyansial112@gmail.com>
2023-07-12 23:35:00 +0500

    profile-card-starter

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <h1>hello</h1>;
+}
+function Intro() {
+  return <p>hi</p>;
+}
+function SkillList() {
+  return <span>yo</span>;
+}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
